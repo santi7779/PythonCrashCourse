d4805152614d886c2141c0bf7fd6ab74e25672e0
santi7779 <marksadlerba@gmail.com>
2020-02-07 20:01:21 -0500

    Python Crash Course

--- Chapter10/file_reader.py
+++ Chapter10/file_reader.py
@@ -1,5 +1,19 @@
+# with open('pi_digits.txt') as file_object:
+#     contents = file_object.read()
+# print(contents.rstrip())
+
+filename = 'pi_digits.txt'
+
 with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-print(contents)
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
+
+
+
+
+
 
 
